2081a13ea5cd7393fe24f35b46d463134e2eb294
Ayush Anand <ayush.anand1110@gmail.com>
2021-01-07 16:32:02 +0530

    initial commit

--- EmailBot.py
+++ EmailBot.py
@@ -1,8 +1,8 @@
 import smtplib
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login('sender_mail@gmai,com', 'password')
+server.login('sender_mail@gmail.com', 'password')
 server.sendmail('sender_mail@gmail.com',
                 'receiver_mail@gmail.com',
-                'message which you want to send'
+                'type message here which you want to send'
                 )
